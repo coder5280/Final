e6bfa6201eaf21346ea9846bb7c7c921510a9b3f
azamat Khadikov <khadikovazamat@gmail.com>
2023-03-10 22:31:03 -0700

    Problem66

---
+++ Problem66/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Sum(m, n);
+
+void Sum(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
